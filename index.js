e900b038e2a960985dd6a3a1d3863f0fbd5de9ef
weider86 <weider86@gmail.com>
2022-06-20 21:13:41 -0300

    wip

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const crud = {
   posts: JSON.parse(postsJson),
   create({ id, content }) {
     const post = { id, content };
+    crud.posts.map((item) =>
+      item.id === post.id
+        ? console.log('Erron on creating post: id already exusts.')
+        : false
+    );
+
     crud.posts.push(post);
+
     writeFileSync('./db.json', JSON.stringify(crud.posts), {
       encoding: 'utf-8',
     });
@@ -17,15 +24,40 @@ const crud = {
     crud.posts = readFileSync('./db.json', { encoding: 'utf-8' });
     return crud.posts;
   },
-  update() {},
+  update({ id, content }) {
+    const post = { id, content };
+
+    crud.posts.find((item) =>
+      item.id === post.id
+        ? (item.content = post.content)
+        : console.log('Erron on updating post: id does not exusts.')
+    );
+
+    writeFileSync('./db.json', JSON.stringify(crud.posts), {
+      encoding: 'utf-8',
+    });
+  },
+  delete({ id }) {
+    const post = { id };
+
+    const newArr = crud.posts.filter((item) => {
+      return item.id !== post.id;
+    });
+
+    writeFileSync('./db.json', JSON.stringify(newArr), {
+      encoding: 'utf-8',
+    });
+  },
 };
 
 // Create
-crud.create({ id: 6, content: 'good dawn 44' });
-
-// Read
-console.log(`read()::: ${crud.read()}`);
+// crud.create({ id: 1, content: 'hello people!' });
 
 // Update
+// crud.update({ id: 2, content: 'hi guys!' });
 
 // Delete
+// crud.delete({ id: 8 });
+
+// Read
+console.log(JSON.parse(crud.read()));
